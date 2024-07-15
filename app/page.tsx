3b8d4db4ac9b55f0ca1a391a04485f17cfdde95e
Victor Martinez <tantus_10@hotmail.com>
2024-07-14 23:51:59 -0600

    commit2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "TailAdmin | E-commerce Dashboard Tempalte",
+  title: "E-commerce Dashboard",
   description: "This is Home for TailAdmin",
   // other metadata
 };
